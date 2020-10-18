208c6e85fc6e4cb624d3f94293423175b28b6c66
marioanloru <marioanloru@gmail.com>
2020-10-18 18:43:12 +0200

    Added dist folder and start script

---
+++ dist/js/rutas.bae23367.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["rutas"],{dffb:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("This is an routes page")])])}],u=t("2877"),a={},c=Object(u["a"])(a,r,s,!1,null,null,null);n["default"]=c.exports}}]);
+//# sourceMappingURL=rutas.bae23367.js.map
